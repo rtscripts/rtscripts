3ca5777b0f1f804eaf552730cb2b1e605b3e37e0
Blake E <electblake@gmail.com>
2020-05-23 15:59:59 -0400

    logger refactoring

--- packages/rtorrent-commands/src/commands/rtscripts.test.ts
+++ packages/rtorrent-commands/src/commands/rtscripts.test.ts
@@ -2,10 +2,11 @@ import rtscripts from "./rtscripts"
 import { testTorrentFactory } from "./test-helpers"
 import temp from "temp"
 
-import logger from "../logger"
+import { logger as makeLogger } from "../logger"
 
 describe("rtscripts", () => {
   jest.mock("yargs")
+  const logger = makeLogger()
   console.log = jest.fn()
   logger.level = "critical"
   const tempFolders: string[] = []
